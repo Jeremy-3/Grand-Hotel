12b5e050cfb3f3b8a1fa6349cc08246dea1e725d
Elviscryptic1981 <Elviskimani99@gmail.com>
2024-08-13 14:47:21 +0300

    updateNavBar

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+  
     <> 
+    
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
