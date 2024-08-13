2d38a10e6d96eeb9c5b9b8bcbca65924cef148f4
Tony Blair Maina <blairpeters13@gmail.com>
2024-08-14 01:47:56 +0300

    Adds Title to the slideshow.

--- src/component/Slideshow.jsx
+++ src/component/Slideshow.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
-const Slideshow = ({ images, interval = 3000 }) => {
+const Slideshow = ({ images, interval = 3000, title, paragraph }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   useEffect(() => {
     const slideInterval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, interval);
 
-    // Clear the interval when the component unmounts
+    // Clears the interval when the component unmounts
     return () => clearInterval(slideInterval);
   }, [images.length, interval]);
 
@@ -18,6 +18,12 @@ const Slideshow = ({ images, interval = 3000 }) => {
         alt={`Slide ${currentIndex + 1}`}
         className="w-full h-full object-cover"
       />
+      {paragraph && <p>{paragraph}</p>}
+      {title && (
+        <h1 className="absolute inset-0 flex items-center justify-center text-white text-6xl md:text-6xl lg:text-8xl font-bold">
+          {title}
+        </h1>
+      )}
     </div>
   );
 };
