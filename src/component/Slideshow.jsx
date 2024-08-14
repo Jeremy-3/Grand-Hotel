3d9dac41c7a599345410577b979b48a481e76547
Tony Blair Maina <blairpeters13@gmail.com>
2024-08-14 13:14:39 +0300

    tries unsuccessfully to add a paragrapgh to the slideshow.

--- src/component/Slideshow.jsx
+++ src/component/Slideshow.jsx
@@ -18,11 +18,16 @@ const Slideshow = ({ images, interval = 3000, title, paragraph }) => {
         alt={`Slide ${currentIndex + 1}`}
         className="w-full h-full object-cover"
       />
-      {paragraph && <p>{paragraph}</p>}
+
       {title && (
         <h1 className="absolute inset-0 flex items-center justify-center text-white text-6xl md:text-6xl lg:text-8xl font-bold">
           {title}
         </h1>
+      )}
+      {paragraph && (
+        <p className="text-lg md:text-xl lg:text-2xl text-blue-800">
+          {paragraph}
+        </p>
       )}
     </div>
   );
