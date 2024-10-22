8325f0b2c39da939fdca7460b11a2c35e966c5a9
+ <+>
2024-10-22 22:25:47 +0300

    now using render server

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5555', 
+        target: 'https://group-propject-backend.onrender.com', 
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), 
       },
